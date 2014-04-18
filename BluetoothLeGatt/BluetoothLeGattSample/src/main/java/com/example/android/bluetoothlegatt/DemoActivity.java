bff42ab4719c1462233f7dd17f6aab26add75467
Morrison Chang <morrison.chang@gmail.com>
2014-04-17 20:16:09 -0400

    Initial version

---
+++ BluetoothLeGatt/BluetoothLeGattSample/src/main/java/com/example/android/bluetoothlegatt/DemoActivity.java
@@ -0,0 +1,25 @@
+package com.example.android.bluetoothlegatt;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * Created by jack.mann on 4/15/14.
+ */
+public class DemoActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+}
